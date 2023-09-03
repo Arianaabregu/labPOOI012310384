38b1897709bd61ae715723bb8e70b66cfe3b5123
AryancYs <133304026+AryancYs@users.noreply.github.com>
2023-09-02 21:42:04 -0500

    Add files via upload

---
+++ Ejericicio2.java
@@ -0,0 +1,18 @@
+
+//2.Escriba una sentencia de Java que imprima true (verdadero), si num es un número impar y positivo. 
+//*********************
+
+public class Main
+{
+	public static void main(String[] args) {
+    int num = 7;
+
+    if (num > 0 && num % 2 != 0) {
+    System.out.println("true");
+} else {
+    System.out.println("false");
+}
+	
+	
+	}
+}
