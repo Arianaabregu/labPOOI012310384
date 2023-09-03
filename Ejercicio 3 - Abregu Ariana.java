e6e0ef8d4e4664b668a73a827a12af7e5d3280b7
AryancYs <133304026+AryancYs@users.noreply.github.com>
2023-09-02 21:56:24 -0500

    Add files via upload

---
+++ Ejercicio 3 - Abregu Ariana.java
@@ -0,0 +1,15 @@
+//3. Escriba una sentencia de Java que imprima true (verdadero), si tanto numA como numB son numeros positivos. 
+//*********************
+
+
+public class Main {
+    public static void main(String[] args) {
+        double numA = 10.5; 
+        double numB = 7.0;  
+
+        if (numA > 0 && numB > 0) {
+            System.out.println("true");
+        }
+    }
+}
+
